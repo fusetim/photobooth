20333e5e40834151dc99c48e3881ee02e3bf65fc
FuseTim <fusetim.log@gmx.com>
2025-07-13 18:41:55 +0200

    add: SolidJS dep + GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://fusetim.github.io',
+  base: '/photobooth',
+  output: 'static',
+  integrations: [solidJs()]
+});
